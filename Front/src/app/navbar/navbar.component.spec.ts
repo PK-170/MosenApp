f3e90674c8d349a4aac821b1722e56b207c0a8f4
PK-170 <pawan59c@gmail.com>
2025-01-21 23:50:24 +1100

    added prop

--- Front/src/app/navbar/navbar.component.spec.ts
+++ Front/src/app/navbar/navbar.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
+ 
   let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
