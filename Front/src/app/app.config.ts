6eb8ae864548f1d8fdc34137dca202c06a08c3d8
PK-170 <pawan59c@gmail.com>
2025-02-20 01:43:29 +1100

    added prop

--- Front/src/app/app.config.ts
+++ Front/src/app/app.config.ts
@@ -3,6 +3,3 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
