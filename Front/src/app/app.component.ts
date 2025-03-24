a60ffbaa07d8464558f6d24b3b52aa34db3a8920
PK-170 <pawan59c@gmail.com>
2025-03-25 00:04:05 +1100

    added prop

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -8,6 +8,4 @@ import { NavbarComponent } from './navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
-}
+
